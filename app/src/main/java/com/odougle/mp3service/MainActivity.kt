a55c05cb5634e7542d7186dee214d6576d6ad500
douguizilla <douglasgommez@gmail.com>
2021-09-01 20:39:04 -0300

    Initial commit

---
+++ app/src/main/java/com/odougle/mp3service/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.odougle.mp3service
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.odougle.mp3service.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding :  ActivityMainBinding by lazy{
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
