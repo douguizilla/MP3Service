1dfdc13dbf9cb47e5be07745bfb728cfdb63aa72
douguizilla <douglasgommez@gmail.com>
2021-09-02 22:16:13 -0300

    implemented onCreate, updateScreen, play, pause, stop

--- app/src/main/java/com/odougle/mp3service/Mp3Activity.kt
+++ app/src/main/java/com/odougle/mp3service/Mp3Activity.kt
@@ -6,14 +6,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.provider.MediaStore
+import android.text.format.DateUtils
 import android.widget.AdapterView
 import android.widget.SimpleCursorAdapter
 import androidx.loader.app.LoaderManager
 import com.odougle.mp3service.databinding.ActivityMp3Binding
 import com.odougle.mp3service.services.Mp3Service
+import java.util.*
 
 class Mp3Activity : AppCompatActivity(), ServiceConnection, AdapterView.OnItemClickListener,
     LoaderManager.LoaderCallbacks<Cursor> {
+
+    private val binding: ActivityMp3Binding by lazy {
+        ActivityMp3Binding.inflate(layoutInflater)
+    }
 
     private var service: Mp3Service? = null
     private var music: String = ""
@@ -37,12 +43,51 @@ class Mp3Activity : AppCompatActivity(), ServiceConnection, AdapterView.OnItemCl
     private lateinit var handler: Handler
     private lateinit var threadProgress: Runnable
 
-    private val binding: ActivityMp3Binding by lazy {
-        ActivityMp3Binding.inflate(layoutInflater)
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        handler = Handler()
+        threadProgress = object : Thread(){
+            override fun run() {
+                updateScreen()
+                if(service?.totalTime ?: 0 > service?.elapsedTime ?: 0){
+                    handler.postDelayed(this, 1000)
+                }
+            }
+        }
+        binding.btnPlay.setOnClickListener { play() }
+        binding.btnPlay.setOnClickListener { pause() }
+        binding.btnStop.setOnClickListener { stop() }
     }
+
+    private fun updateScreen(){
+        music = service?.currentSong ?: ""
+        binding.txtSong.text = music
+        val elapsedTime = service?.elapsedTime ?: 0
+        binding.txtTime.text = DateUtils.formatElapsedTime(elapsedTime.toLong() / 1000)
+        binding.progressBar.max = service?.totalTime ?: 0
+        binding.progressBar.progress = elapsedTime
+    }
+
+    private fun play(){
+        handler.removeCallbacks(threadProgress)
+        if(music.isNotBlank()){
+            service?.play(music)
+            handler.post(threadProgress)
+        }
+    }
+
+    private fun pause(){
+        service?.pause()
+        handler.removeCallbacks(threadProgress)
+    }
+
+    private fun stop(){
+        service?.stop()
+        handler.removeCallbacks(threadProgress)
+        binding.progressBar.progress = 0
+        binding.txtTime.text = DateUtils.formatElapsedTime(0)
+    }
+
+
 }
