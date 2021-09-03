c8f677fbe7c1e16d0ecc76a6a3b40a1f1f6daba4
douguizilla <douglasgommez@gmail.com>
2021-09-02 21:47:43 -0300

    set interfaces that Mp3Activity.kt will implement

--- app/src/main/java/com/odougle/mp3service/Mp3Activity.kt
+++ app/src/main/java/com/odougle/mp3service/Mp3Activity.kt
@@ -1,14 +1,20 @@
 package com.odougle.mp3service
 
+import android.content.ServiceConnection
+import android.database.Cursor
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.AdapterView
+import androidx.loader.app.LoaderManager
 import com.odougle.mp3service.databinding.ActivityMp3Binding
 
-class Mp3Activity : AppCompatActivity() {
+class Mp3Activity : AppCompatActivity(), ServiceConnection, AdapterView.OnItemClickListener,
+    LoaderManager.LoaderCallbacks<Cursor> {
 
-    private val binding : ActivityMp3Binding by lazy{
+    private val binding: ActivityMp3Binding by lazy {
         ActivityMp3Binding.inflate(layoutInflater)
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
