560a69477a8326ee071425d1613167baa6b2eaee
douguizilla <douglasgommez@gmail.com>
2021-09-02 21:52:27 -0300

    start to implement Mp3Activity.kt

--- app/src/main/java/com/odougle/mp3service/Mp3Activity.kt
+++ app/src/main/java/com/odougle/mp3service/Mp3Activity.kt
@@ -4,12 +4,22 @@ import android.content.ServiceConnection
 import android.database.Cursor
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.AdapterView
 import androidx.loader.app.LoaderManager
 import com.odougle.mp3service.databinding.ActivityMp3Binding
+import com.odougle.mp3service.services.Mp3Service
 
 class Mp3Activity : AppCompatActivity(), ServiceConnection, AdapterView.OnItemClickListener,
     LoaderManager.LoaderCallbacks<Cursor> {
+
+    private var service: Mp3Service? = null
+    private var music: String = ""
+    private val columns = arrayOf(
+        MediaStore.MediaColumns.DISPLAY_NAME,
+        MediaStore.MediaColumns.DATA,
+        MediaStore.MediaColumns._ID
+    )
 
     private val binding: ActivityMp3Binding by lazy {
         ActivityMp3Binding.inflate(layoutInflater)
